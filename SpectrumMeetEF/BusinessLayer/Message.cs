301f263d59223d3707e7a2e67fc63b139a346498
dalia <dg25485@email.vccs.edu>
2024-03-22 10:05:20 -0400

    i made a hyperlink

---
+++ SpectrumMeetEF/BusinessLayer/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpectrumMeetEF
+{
+    public partial class Message
+    {
+        public SpectrumMeetEntities db = new SpectrumMeetEntities();
+        
+
+
+
+    }
+}
--- SpectrumMeetEF/SpectrumMeet_Model.Context.cs
+++ SpectrumMeetEF/SpectrumMeet_Model.Context.cs
@@ -41,9 +41,9 @@ namespace SpectrumMeetEF
         public virtual DbSet<Role> Roles { get; set; }
         public virtual DbSet<SupportLevel> SupportLevels { get; set; }
         public virtual DbSet<sysdiagram> sysdiagrams { get; set; }
-        public virtual DbSet<Tag> Tags { get; set; }
         public virtual DbSet<User> Users { get; set; }
         public virtual DbSet<Message> Messages { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
     
         public virtual int sp_alterdiagram(string diagramname, Nullable<int> owner_id, Nullable<int> version, byte[] definition)
         {
