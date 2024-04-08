b068f22fd670ceed2f3e0a9d04e3560e320bb998
RaeofSunshine95 <RaeSun95@gmail.com>
2024-04-08 16:59:42 -0400

    Nested replies! While the implementation is a bit clunky and I need to fix the back button's behavior, the main feature is OVER WITH it's incredibly stupid how I had to do this, but it works, so I'm not going to complain too much. May strain the database when rendering a lot of messages at a time due to every message making a separate query, but shouldn't be too big a deal.

--- SpectrumMeetEF/BusinessLayer/Message.cs
+++ SpectrumMeetEF/BusinessLayer/Message.cs
@@ -6,6 +6,7 @@ using System.Data.Entity.Migrations;
 using System.Data.SqlClient;
 using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,7 +15,7 @@ namespace SpectrumMeetEF
     public partial class Message
     {
         public SpectrumMeetEntities db = new SpectrumMeetEntities();
-        
+
         //Function takes two parameters, the message it's modifying and the account (found via session id)
         //Controller action will call this function using the session's accountID
         public void MarkAsRead(Message m, Account a)
@@ -35,15 +36,21 @@ namespace SpectrumMeetEF
                         m.MessageReadStatus = true;
                         db.Messages.AddOrUpdate(m);
                     }
-                }  
+                }
             }
         }
         public List<Message> GetAllChildren()
         {
+            //Okay let's try to break this down.
+            //First I make an empty list for later
             List<Message> children = new List<Message>();
+            //Then I create an sqlparameter for use in the upcoming database query
             SqlParameter anchorId = new SqlParameter("@anchorId", MessageID);
+            //I make the query for the current message's ID and it returns to me all children in the hierarchy
             var rh = db.Database.SqlQuery<Message>("EXEC RecursiveHierarchy @anchorId", anchorId);
-            foreach (var child in rh )
+            //Unfortunately they aren't actually message objects in this form, so we have to iterate through the
+            //result and make all the properties ourselves. Hacky but functional.
+            foreach (var child in rh)
             {
                 Message m = new Message();
                 m.GroupID = child.GroupID;
@@ -57,7 +64,72 @@ namespace SpectrumMeetEF
                 m.User = db.Users.Find(child.AccountID);
                 children.Add(m);
             }
-            return children;
-        }
+            //I then group the children by parent message ID, probably not useful so I might remove it.
+            children.GroupBy(m => m.ParentMessageID);
+            //I make a new list of the IDs of all messages that should be parents
+            List<int> ParentIDs = new List<int>();
+            //Populate the list with everything that isn't a direct descendant or already on the list
+            foreach (var child in children)
+            {
+                if (child.ParentMessageID != MessageID && !ParentIDs.Contains((int)child.ParentMessageID))
+                {
+                    ParentIDs.Add((int)child.ParentMessageID);
+                }
+            }
+            //Make a new list of messages that are supposed to be parents
+            List<Message> ParentMessages = new List<Message>();
+            //Populate that list by crossreferencing the IDs from earlier.
+            foreach(var child in children)
+            {
+                foreach(var id in ParentIDs)
+                {
+                    if (child.MessageID == id)
+                    {
+                        ParentMessages.Add(child);
+                    }
+                }
+            }
+            //Make a copy of the children list so I can modify it as I choose, will be important later.
+            List<Message> modifiedChildren = new List<Message>(children);
+            //Make a list of messages that should be replies
+            List<Message> replies = new List<Message>();
+            //Populate that list with replies by parent message
+            foreach (var parent in ParentMessages)
+            {
+                foreach (var child in children)
+                {
+                    if (child.ParentMessageID == parent.MessageID)
+                    {
+                        modifiedChildren.Remove(child);
+                        replies.Add(child);
+                    }
+                }
+            }
+            //iterate through children one more time (this is probably wrong and I should use the length of modified children
+            //so that the data is consistent
+            //but it populates modified-children with the replies one ahead of its parent
+            foreach (var child in children)
+            {
+                foreach(var reply in replies)
+                {
+                    if(reply.ParentMessageID == child.MessageID && !modifiedChildren.Contains(child))
+                    {
+                        modifiedChildren.Insert(modifiedChildren.IndexOf(child)+1, reply);
+                    }
+                }
+            }
+            //had to add this to filter out the duplicate messages appearing from the recursive query
+            //this function sucks but I'm not changing it since it works now.
+            //Hope this made even a slight amount of sense.
+            //- Rachel
+            for(int i=0;i<modifiedChildren.Count();i++)
+            {
+                if (modifiedChildren[i].ParentMessageID != MessageID)
+                {
+                    modifiedChildren.Remove(modifiedChildren[i]);
+                }
+            }
+            return modifiedChildren;
+        }          
     }
 }
