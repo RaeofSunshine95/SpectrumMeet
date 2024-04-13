98323705564d9154144a3f56803c50327a126ca6
RaeofSunshine95 <RaeSun95@gmail.com>
2024-04-13 19:38:03 -0400

    Condition Database and User Search Currently only searches by location Going to add condition based search soonTM I need to stop staring at this for the day.

---
+++ SpectrumMeetMVC/Areas/Condition/ConditionAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SpectrumMeetMVC.Areas.Condition
+{
+    public class ConditionAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Condition";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Condition_default",
+                "Condition/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
