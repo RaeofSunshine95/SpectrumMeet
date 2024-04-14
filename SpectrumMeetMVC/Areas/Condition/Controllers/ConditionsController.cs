103f45c4accf16bce1fd9938f0bd1fc70d4e2ef9
RaeofSunshine95 <RaeSun95@gmail.com>
2024-04-13 20:36:27 -0400

    I really hope this fixes it

---
+++ SpectrumMeetMVC/Areas/Condition/Controllers/ConditionsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SpectrumMeetEF;
+
+namespace SpectrumMeetMVC.Areas.Condition.Controllers
+{
+    public class ConditionsController : Controller
+    {
+        private SpectrumMeetEntities db = new SpectrumMeetEntities();
+
+        // GET: Condition/Conditions
+        public ActionResult Index()
+        {
+            return View(db.Conditions.ToList());
+        }
+
+        // GET: Condition/Conditions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SpectrumMeetEF.Condition condition = db.Conditions.Find(id);
+            if (condition == null)
+            {
+                return HttpNotFound();
+            }
+            return View(condition);
+        }
+
+        // GET: Condition/Conditions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Condition/Conditions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ConditionID,Name,Description")] SpectrumMeetEF.Condition condition)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Conditions.Add(condition);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(condition);
+        }
+
+        // GET: Condition/Conditions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SpectrumMeetEF.Condition condition = db.Conditions.Find(id);
+            if (condition == null)
+            {
+                return HttpNotFound();
+            }
+            return View(condition);
+        }
+
+        // POST: Condition/Conditions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ConditionID,Name,Description")] SpectrumMeetEF.Condition condition)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(condition).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(condition);
+        }
+
+        // GET: Condition/Conditions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SpectrumMeetEF.Condition condition = db.Conditions.Find(id);
+            if (condition == null)
+            {
+                return HttpNotFound();
+            }
+            return View(condition);
+        }
+
+        // POST: Condition/Conditions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SpectrumMeetEF.Condition condition = db.Conditions.Find(id);
+            db.Conditions.Remove(condition);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
